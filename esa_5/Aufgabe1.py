7282ba1d428a7a3cad9f81045b67315744defc40
Mia Seemann <mia@5r4.de>
2025-09-27 19:40:27 +0200

    update lösung

--- esa_5/Aufgabe1.py
+++ esa_5/Aufgabe1.py
@@ -16,16 +16,13 @@ for wert in werte:
     a = wert[0];
     b = wert[1];
     e = verknuepfung(a,b)
-
+   # Ohne Übersetzung   
    # print (str(a) + "\t" + str(b) + "\t" + e)
     
     #Mit übersetzung 
-
     print(str(uebersetzung(a)) + "\t" +str(uebersetzung(b)) + "\t" + e)
 
 
-#zahl1 = 0
-#zahl = int(input("Bitte Zahl eingeben:"))
 
 
 
